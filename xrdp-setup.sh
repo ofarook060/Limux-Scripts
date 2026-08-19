5dbea0728701efbd9b9968a0b2902ecd21f201cf
Omer Farook <79522360+ofarook060@users.noreply.github.com>
2026-08-19 20:49:35 +0630

    Update xrdp setup script to use sudo for commands

--- xrdp-setup.sh
+++ xrdp-setup.sh
@@ -8,11 +8,11 @@ sed -i '/exec \/bin\/sh \/etc\/X11\/Xsession/d' /etc/xrdp/startwm.sh
 
 # Create an alias to restart the xrdp server in the future by typing 'xrdpstart', this is inverse to the termux alias as to not cause conflicts
 
-echo "alias xsrt='service xrdp stop && service xrdp start && ifconfig'" >> ~/.bashrc
+echo "alias xsrt='sudo service xrdp stop && sudo service xrdp start && ifconfig'" >> ~/.bashrc
 
 # Create an alias to stop the xrdp server by typing xrdpstop   
 
-echo "alias xstp='service xrdp stop'" >> ~/.bashrc && source ~/.bashrc
+echo "alias xstp='sudo service xrdp stop'" >> ~/.bashrc && source ~/.bashrc
 
 # Let user know installation is complete
 clear && echo "Xrdp is installed. To use, type xrdpstart and connect to the WLAN0 local IP with your PC."
